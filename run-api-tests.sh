0f072b898d37492ac4ae773004957bb4616b349e
Ash <ashkumark@gmail.com>
2023-11-07 17:07:30 +0000

    Updated jenkinsfile

--- run-api-tests.sh
+++ run-api-tests.sh
@@ -1,7 +1,10 @@
 # JOB_NAME is the name of the project of this build. This is the name you gave your job. It is set up by Jenkins
 COMPOSE_ID=${JOB_NAME:-local}
+echo $COMPOSE_ID
+
 # Remove Previous Stack
 docker-compose -p $COMPOSE_ID rm -f
+
 # Starting new stack environment
 docker-compose -f docker-compose-api.yaml up -p $COMPOSE_ID -d --no-color --build
 docker-compose -f docker-compose-api.yaml run -p $COMPOSE_ID -e TYPE="@API" api-test-service
